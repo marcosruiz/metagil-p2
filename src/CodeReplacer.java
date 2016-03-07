903658f9018ff2e83c244ddd1b17fb3158880b04
Marcos Ruiz García <648045@unizar.es>
2016-03-07 16:23:28 +0100

    Initial commit

---
+++ src/CodeReplacer.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.Scanner;
+
+/** Replace %NAME% with requested id, and %INITIALS% w/"initials" version id.*/
+public class CodeReplacer {
+	private static String TEMPLATE_DIR = "user.dir";
+	static String sourceTemplate;
+	static String code;
+	static String initcode;
+	
+	
+	
+	public CodeReplacer(String reqId, String in, PrintWriter out){
+		String templateDir = System.getProperty(TEMPLATE_DIR, "");
+		try {
+			Scanner scanner = new Scanner(new File("./template.html"));
+			String line;
+			line = scanner.nextLine();
+			// Until template is finished
+			while((line!="") && (line!=null)){
+				sourceTemplate += line + "\n";
+				line = scanner.nextLine();
+			}
+			scanner.close();
+		} catch (Exception e) { }
+	}
+	/**
+	* @param reqId java.lang.String
+	* @param out java.io.PrintWriter
+	* @exception IOException The exception description.
+	*/
+	public void substitute(String reqId, PrintWriter out) throws IOException	{
+		try {
+			String template = new String(sourceTemplate);
+			// Substitute for %CODE%
+			int templateSplitBegin = template.indexOf("%NAME%");
+			int templateSplitEnd = templateSplitBegin + 6;
+			String templatePartOne = new String(template.substring(0, templateSplitBegin));
+			String templatePartTwo = new String(template.substring(templateSplitEnd, template.length()));
+			code = new String(reqId);
+			template = new String(templatePartOne+code+templatePartTwo);
+			// Substitute for %ALTCODE%
+			templateSplitBegin = template.indexOf("%INITIALS%");
+			templateSplitEnd = templateSplitBegin + 10;
+			templatePartOne = new String(template.substring(0, templateSplitBegin));
+			templatePartTwo = new String(template.substring(templateSplitEnd, template.length()));
+			String[] str = code.split(" ");
+			initcode = "";
+			for (String s: str) 
+				if (s.length() > 0)
+					initcode += s.charAt(0);
+			out.print(templatePartOne+initcode+templatePartTwo);
+		} catch (Exception e) {
+			System.out.println("Error in substitute()");
+		}
+		out.flush();
+		out.close();
+	}
+}
---
+++ test/CodeReplacerTest.java
@@ -0,0 +1,61 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import code.CodeReplacer;
+import org.junit.*;
+
+public class CodeReplacerTest {
+	
+	private static StringWriter strOut;
+	private static String stringSolucion;
+	static PrintWriter out;
+	private CodeReplacer cr;
+	
+	@Before
+	public void inicializacion() throws IOException {
+		cr = new CodeReplacer();
+		strOut = new StringWriter();	
+		out = new PrintWriter(strOut);
+	}
+	@After
+	public void finalizacion(){
+		assertEquals(stringSolucion, strOut.toString());
+	}
+	
+	@Test
+	public void pruebaValido() throws IOException {
+		stringSolucion = "XXXcXXXcambioXXX%INITIALS%XXX%NAME%XXX\nfdsfsd\nfjsdlkfjs\n";
+		cr.substitute("cambio", out);
+	}
+	
+	@Test
+	public void pruebaCadenaVacia() throws IOException {
+		stringSolucion = "XXXXXXXXX%INITIALS%XXX%NAME%XXX\nfdsfsd\nfjsdlkfjs\n";
+		cr.substitute("", out);
+	}
+	
+	@Test
+	public void pruebaNullString() throws IOException {
+		stringSolucion = "";
+		cr.substitute(null, out);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void pruebaNullOut() throws IOException {
+		stringSolucion = "";
+		cr.substitute("cambio", null);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void pruebaNullNull() throws IOException {
+		stringSolucion = "";
+		cr.substitute(null, null);
+	}
+	
+	
+}
